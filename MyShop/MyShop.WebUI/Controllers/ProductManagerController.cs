8709456c5314ca6bd8f8ddab9f55ce04793b2ed4
Shubham <shubham.aakashinfo@gmail.com>
2022-10-13 14:50:50 +0530

    Implemented Generic Repository

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -11,12 +11,14 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
+        //ProductRepository context;
+        //ProductCategoryRepository productCategories;
 
         public ProductManagerController(){
-            context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
         public ActionResult Index()
